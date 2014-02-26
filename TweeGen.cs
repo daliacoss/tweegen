a16bc71f3107731f6147d6c277556b684bc60796
david <cosstropolis@gmail.com>
2014-02-26 16:20:25 -0500

    can convert passage to xml string

--- TweeGen.cs
+++ TweeGen.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using System.IO;
 using System.Text.RegularExpressions;
 
 enum Tag {
@@ -28,26 +30,29 @@ class TweeParser {
 		public string Text;
 		public Dictionary<Tag,bool> Tags;
 		//we can just use the passage title for the link address
-		public /*List<Subpassage>*/string LinkAddress;
+		public string LinkAddress;
 
 		//we'll use this for nested macros maybe?
 		Subpassage Sub;
 	}
 
 	public string Stream;
-	//maybe i'll change the string arrays to regexes, and use them as the keys?
-	public readonly Dictionary<Tag,string[]> TagStrings = new Dictionary<Tag,string[]>(){
-		{Tag.Link, new string[3]{@"\[\[",@"\|",@"\]\]"}},
-		{Tag.Bold, new string[2]{"''","''"}},
-		{Tag.Italic, new string[2]{"//","//"}},
-		{Tag.Underline, new string[2]{"__","__"}},
-		{Tag.Subscript, new string[2]{"~~","~~"}},
-		{Tag.Superscript, new string[2]{@"\^\^",@"\^\^"}},
-		{Tag.Monospace, new string[2]{"{{{","}}}"}},
-		{Tag.OrderedList, new string[1]{"#"}},
-		{Tag.UnorderedList, new string[1]{@"\*"}},
-		{Tag.Macro, new string[2]{"<<",">>"}},
+	public readonly Dictionary<string,string[]> TagStrings = new Dictionary<string,string[]>(){
+		//tags that use the same strings for close and open will be used in regexes
+		//(macros are detected separately)
+		{"link", new string[2]{"[[","]]"}},
+		{"strong", new string[1]{@"\'\'"}},
+		{"em", new string[1]{"//"}},
+		{"underline", new string[1]{"__"}},
+		{"subscript", new string[1]{"~~"}},
+		{"superscript", new string[1]{@"\^\^"}},
+		{"monospace", new string[2]{"{{{","}}}"}},
+		//{"olli", new string[1]{"#"}},
+		//{"ulli", new string[1]{@"\*"}},
+		//we need to escape these so they don't get confused with xml
+		//{"macro", new string[2]{@"<<",@">>"}},
 	};
+	public readonly string MacroName = "macro";
 	//this time the strings are keys - we are more likely to search for "eq" than Operator.Eq
 	public readonly Dictionary<string,Operator> OpStrings = new Dictionary<string,Operator>(){
 		{"eq",Operator.Eq},
@@ -74,7 +79,6 @@ class TweeParser {
 		Dictionary<string,List<Subpassage>> passageTree = new Dictionary<string,List<Subpassage>>();
 
 		//all titles begin with (newline):: and optional whitespace, and end with newline
-		
 		stream = "\n" + stream;
 		MatchCollection matches = Regex.Matches(stream, @"\r*\n+::\s*\w+\r*\n+");
 		char[] charsToStrip = {':',' ','\n','\r'};
@@ -109,25 +113,37 @@ class TweeParser {
 		MatchCollection matches = null;
 
 		//parse all the escape characters...
-		string expBase = "[{0}].*[{1}]", expLeft = "", expRight = "";
+		//string expBase = "[{0}].*[{1}]", expLeft = "", expRight = "";
 
+				//Console.WriteLine(bold.Replace(body, "<strong>$2</strong>"));
+
+		//check for macros first so they don't get confused with xml
+		body = body.Replace("<<", "<"+MacroName+">");
+		body = body.Replace(">>", "</"+MacroName+">");
+
+		//now do the other formatting tags
 		//regex stolen from Milan Negovan's MarkDownSharp:
-		Regex bold = new Regex(@"(\'\') (?=\S) (.+?[*_]*) (?<=\S) \1",
-            RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline | RegexOptions.Compiled);
-		Console.WriteLine(bold.Replace(body, "<strong>$2</strong>"));
-
+		string exp = "({0})" + @" (?=\S) (.+?[*_]*) (?<=\S) \1";
 		foreach (var ts in TagStrings){
 			var s = ts.Value;
 			//add all of the tag strings to the regex
-			if (s.Length > 1) expLeft += "("+s[0]+")";
-			if (s.Length == 2) expRight = "("+s[1]+")" + expRight;
-			else if (s.Length == 3) expRight += s[2];
+			if (s.Length == 1){
+				Regex bold = new Regex(String.Format(exp, s[0]),
+					RegexOptions.IgnorePatternWhitespace | 
+					RegexOptions.Singleline | 
+					RegexOptions.Compiled);
+				body = bold.Replace(body, "<"+ts.Key+">$2"+"</"+ts.Key+">");
+			}
+			if (s.Length == 2){
+				body = body.Replace(s[0],"<"+ts.Key+">");
+				body = body.Replace(s[1],"</"+ts.Key+">");
+			}
 
 		}
-		//Console.WriteLine(String.Format(expBase, expLeft, expRight));
-		//matches = Regex.Matches(body, String.Format(expBase, expLeft, expRight));
-		//parseMatch(matches);
-		//recursiveMatch(body, String.Format(expBase, expLeft, expRight));
+
+		//must wrap xml in a parent element
+		body = "<passage>" + body + "</passage>";
+		//parse xml
 
 		sp.Text = body;
 		subpassages.Add(sp);
@@ -155,12 +171,14 @@ class TweeParser {
 class TweeGen {
 	static void Main(){
 		TweeParser tp = new TweeParser();
-		//:: End\nyes
-		var tree = tp.Parse("\n::Start\r\n''bl<<ah [[link|passage]]>>'' __etc__\n");
+		//:: End
+		var tree = tp.Parse("\n::Start\r\nthis text is ''bold''\n");
 		foreach (var i in tree){
 			Console.WriteLine(i.Value[0].Text);
 		}
 		
+		var e = 3;
+		if ((e+=1) == 4) Console.WriteLine(e);
 		/*MatchCollection mc = Regex.Matches(@"<<ah >>[[link|passage]]", @"[(\[\[)(<<)].*[(\]\])(>>)]");
 		foreach (var m in mc){
 			//MatchCollection
